bbf326c2676ddece151cea6e202da14f3ccbe351
Adrien <adrien@gn2.fr>
2009-10-19 10:47:22 +0100

    Javascript lib updates

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.namespace(:admin) do |admin|
     admin.resources :settings
   end
-  
+
   map.admin '/admin', :controller => 'admin/dashboard'
   map.admin_dashboard '/admin/dashboard', :controller => 'admin/dashboard'
 end
